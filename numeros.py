5c0cf78f7cb7b8cd45a6757aa3e2ee8a836bd81d
susanafigueiredo3112-byte <susanafigueiredo3112@gmail.com>
2026-04-28 21:50:34 +0100

    teste
    teste

--- numeros.py
+++ numeros.py
@@ -10,5 +10,7 @@ numero2 = int(input("Introduz outro número: "))
 if numero1 == numero2:
     print("Os números introduzidos são iguais.")
 else:
-    print("Os números introduzidos são diferentes.")
+    print("Os números introduzidos são diferentes.") # Fim do programa
+    
+
     
