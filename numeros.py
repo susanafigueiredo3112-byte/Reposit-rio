52a65185291c744810b1421ab213546a37b16c28
unknown <seuemail@exemplo.com>
2026-04-28 21:16:23 +0100

    Adicionar mensagem para números diferentes

--- numeros.py
+++ numeros.py
@@ -3,3 +3,5 @@ numero2 = int(input("Introduz outro número: "))
 
 if numero1 == numero2:
     print("Os números introduzidos são iguais.")
+else:
+    print("Os números introduzidos são diferentes.")
