6f6453e9203f262d19f90a150ecbfd1b6731cde4
unknown <seuemail@exemplo.com>
2026-04-28 21:32:06 +0100

    Adicionar comentários ao código

--- numeros.py
+++ numeros.py
@@ -1,7 +1,14 @@
+# Programa: Verificar se dois números são iguais ou diferentes
+
+# Pedir o primeiro número ao utilizador
 numero1 = int(input("Introduz um número: "))
+
+# Pedir o segundo número ao utilizador
 numero2 = int(input("Introduz outro número: "))
 
+# Verificar se os números são iguais ou diferentes
 if numero1 == numero2:
     print("Os números introduzidos são iguais.")
 else:
     print("Os números introduzidos são diferentes.")
+    
